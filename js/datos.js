c36fd1ae41dd320834cd5775d29385bb922e05a3
iaraszuir <iaraszuir@gmail.com>
2022-03-06 10:16:27 +0100

    lista filtradas añadidas

---
+++ js/datos.js
@@ -0,0 +1,20 @@
+let listaRutinas = new Array(
+    {
+        idRutina: 1,
+        titulo: 'Comer',
+        prioridad: 'diaria'
+    },
+
+    {
+        idRutina: 2,
+        titulo: 'Viajar',
+        prioridad: 'mensual'
+    },
+
+    {
+        idRutina: 3,
+        titulo: 'Estudiar Bootcamp',
+        prioridad: 'urgente'
+    },
+
+)
---
+++ js/funciones.js
@@ -0,0 +1,99 @@
+const divLista = document.getElementById('tareasPending');
+const addInputTarea = document.getElementById('inputTarea')
+const selector = document.getElementById('selectorPrioridadTarea')
+const labelTarea = document.getElementById('labelTarea')
+const inputPrioridad = document.getElementById('inputPrioridad')
+const btnGuardar = document.getElementById('btnGuardar')
+const btnDelete = document.querySelector('#tareasPending button')
+const selectorBusqueda = document.getElementById('selectorPrioridadBusqueda')
+const inputTareaBuscada = document.getElementById('inputTareaBusqueda')
+
+
+function pintarTarea(pTarea) {
+    const nombreTarea = document.createElement('h2')
+    nombreTarea.innerText = pTarea.titulo;
+    const btnDelete = document.createElement('button')
+    btnDelete.innerText = 'Eliminar'
+    const prioridadTarea = document.createElement('p')
+    prioridadTarea.innerHTML = ''
+    const divTareasPending = document.createElement('div')
+
+    divTareasPending.appendChild(nombreTarea)
+    divTareasPending.appendChild(btnDelete)
+
+    return divTareasPending;
+}
+
+
+
+function printListaTareas(pListaTareas) {
+    divLista.innerHTML = '';
+
+    for (tarea of pListaTareas) {
+        const divTarea = pintarTarea(tarea)
+        divLista.appendChild(divTarea)
+    }
+
+}
+
+
+
+btnGuardar.addEventListener('click', guardaCampolabel)
+
+function guardaCampolabel(event) {
+
+    const tareaNueva = {
+        idRutina: listaRutinas.length + 1,
+        titulo: addInputTarea.value,
+        prioridad: selector.value,
+    }
+
+    listaRutinas.push(tareaNueva);
+
+
+    const divNuevaTarea = pintarTarea(tareaNueva);
+    divLista.appendChild(divNuevaTarea)
+    return;
+
+}
+
+selectorBusqueda.addEventListener('change', (event) => {
+
+    const priorizados = [];
+    for (tarea of listaRutinas) {
+        if (tarea.prioridad === event.target.value) {
+            priorizados.push(tarea)
+        }
+
+    }
+    printListaTareas(priorizados, listaRutinas)
+})
+
+inputTareaBuscada.addEventListener('input', (event) => {
+
+    const arrFiltradoTarea = [];
+
+    const tareaIncluida = event.target.value.toLowerCase();
+
+    for (tarea of listaRutinas) {
+        if (tarea.titulo.toLowerCase().includes(tareaIncluida) && !arrFiltradoTarea.includes(tarea)) {
+
+            arrFiltradoTarea.push(tarea);
+        }
+
+    }
+    printListaTareas(arrFiltradoTarea, listaRutinas)
+})
+
+
+//Borrar
+//Crear el boton
+//Borrar la tarea   
+//Borrar el padre del boton
+//Borrar la tarea del array
+
+/* if(tarea.prioridad===mensual){
+    divLista.style.backgroundColor
+} */
+
+printListaTareas(listaRutinas)
